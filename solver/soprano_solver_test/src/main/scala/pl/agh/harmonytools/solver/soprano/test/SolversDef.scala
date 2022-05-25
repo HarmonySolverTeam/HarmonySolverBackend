11eb64a7f35ce57523d009310be6827207a89394
Mikołaj Sikora <mikolajsikora98@gmail.com>
2022-05-25 13:49:11 +0200

    HS-192 Genetic Tests

--- solver/soprano_solver_test/src/main/scala/pl/agh/harmonytools/solver/soprano/test/SolversDef.scala
+++ solver/soprano_solver_test/src/main/scala/pl/agh/harmonytools/solver/soprano/test/SolversDef.scala
@@ -3,7 +3,6 @@ package pl.agh.harmonytools.solver.soprano.test
 import pl.agh.harmonytools.exercise.soprano.SopranoExercise
 import pl.agh.harmonytools.solver.SopranoSolution
 import pl.agh.harmonytools.solver.soprano.SopranoSolver
-//import pl.agh.harmonytools.solver.soprano.bayes.BayesNetSopranoSolver
 import pl.agh.harmonytools.solver.soprano.genetic.{SopranoChordGenerator, SopranoGeneticSolver, SopranoHarmonizationProblem}
 
 import scala.collection.mutable.ListBuffer
@@ -18,7 +17,7 @@ object SolversDef {
 
   def solveByGeneticAlgorithm(name: String, exercise: SopranoExercise): List[Double] = {
     val ratings = ListBuffer[Double]()
-    for (_ <- 1 to 5) {
+    for (_ <- 1 to 10) {
       val solution = new SopranoGeneticSolver(exercise, 2000, 2000, 0.2, 0.5, 0.3, new SopranoHarmonizationProblem(exercise)).solve()
       val path = s"solver/soprano_solver_test/src/main/resources/genetic_solutions/$name"
       solution.save(path)
@@ -27,23 +26,12 @@ object SolversDef {
     ratings.toList
   }
 
-//  def solveByBayesNetwork(name: String, exercise: SopranoExercise): List[Double] = {
-//    val ratings = ListBuffer[Double]()
-//    for (_ <- 1 to 10) {
-//      val solution = new BayesNetSopranoSolver(exercise).solve()
-//      val path = s"solver/soprano_solver_test/src/main/resources/bayes_solutions/$name"
-//      solution.save(path)
-//      ratings.append(solution.rating)
-//    }
-//    ratings.toList
-//  }
-
   def solveByHybridAlgorithm(exercise: SopranoExercise): SopranoSolution =
     new SopranoGeneticSolver(exercise, 30, 20, 0.2, 0.5, 0.3, new SopranoHarmonizationProblem(exercise)).solve()
 
   def solveByAll(name: String, exercise: SopranoExercise): Map[String, Any] = {
     Map(
-      "rule_based_system" -> solveByRuleBasedSystem(name, exercise),
+//      "rule_based_system" -> solveByRuleBasedSystem(name, exercise),
       "genetic_algorithm" -> solveByGeneticAlgorithm(name, exercise),
 //      "bayes_network" -> solveByBayesNetwork(name, exercise)
     )
